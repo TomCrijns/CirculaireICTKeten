ea44c79bdd419740bd47c5c48286cdab8ec783d6
Tom Crijns <1816802crijns@zuyd.nl>
2021-06-18 19:45:36 +0200

    Initial code except dashboard because of massive conflict and our unittests

---
+++ CirculaireICTKeten/CirculaireICTKeten/Interfaces/ISendEmailService.cs
@@ -0,0 +1,14 @@
+﻿using CirculaireICTKeten.Services.EmailService;
+using CirculaireICTKeten.Services.EmailService.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CirculaireICTKeten.Interfaces
+{
+    public interface ISendEmailService
+    {
+        void SendMessage(EmailMessage emailMessage);
+    }
+}
